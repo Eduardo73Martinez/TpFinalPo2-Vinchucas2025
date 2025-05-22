14bab5013c5f76b95147dbad5e688f3c64f7c123
martinez-quimey <quimeym374@gmail.com>
2025-05-22 16:59:14 -0300

    agregue un test

---
+++ test/web_vinchucas/MuestraTest.java
@@ -0,0 +1,19 @@
+package web_vinchucas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MuestraTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testPrueba() {
+		fail("Not yet implemented");
+	}
+
+}
