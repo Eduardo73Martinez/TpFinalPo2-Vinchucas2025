f4b657248069df5a3e805ed8d380156773b33040
marti <marti@192.168.0.69>
2025-05-23 17:02:15 -0300

    creo el package zona, ubicacion y organizacion. Las clases vacias y los test vacios

---
+++ test/ar/edu/unq/po2/zonaOrganizacionUbicacion/ZonaCoberturaTest.java
@@ -0,0 +1,19 @@
+package ar.edu.unq.po2.zonaOrganizacionUbicacion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ZonaCoberturaTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
