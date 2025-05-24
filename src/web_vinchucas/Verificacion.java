7ba7eb8f594d3b6a282489eb517256b7328e228d
martinez-quimey <quimeym374@gmail.com>
2025-05-24 18:37:14 -0300

    creada la clase Verificacion empezo a crearse la subclase Verificada

--- src/web_vinchucas/Verificacion.java
+++ src/web_vinchucas/Verificacion.java
@@ -2,4 +2,15 @@ package web_vinchucas;
 
 public abstract class  Verificacion {
 
+	protected Muestra muestra;
+	abstract boolean esVerificada();
+	abstract boolean puedeVotar (Usuario usuario);
+	abstract void verificar(); //recalcula el tipo de verificacion
+	private void setMuestra (Muestra muestra){
+		this.muestra = muestra;
+	}
+	public Verificacion (Muestra muestra) {
+		setMuestra (muestra);
+	}
+	
 }
---
+++ src/web_vinchucas/Verificada.java
@@ -0,0 +1,10 @@
+package web_vinchucas;
+
+public class Verificada extends Verificacion {
+	public boolean esVerificada() {
+		return true;
+	}
+	public Verificada (Muestra muestra) {
+		super (muestra);
+	}
+}
