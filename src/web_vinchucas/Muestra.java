b264b16fa056a415d798223e2e5aae697ddae279
Lucio <Lucio@DESKTOP-MSN6PQ5>
2025-05-28 10:27:07 -0300

    Refactorización de muestra (Extract Method) y avanzo con los test

--- src/web_vinchucas/Muestra.java
+++ src/web_vinchucas/Muestra.java
@@ -8,46 +8,62 @@ import java.util.stream.Collectors;
 public class Muestra {
 	Usuario autor;
 	Foto foto;
-	Ubicacion ubicacion;
+	Ubicación ubicacion;
 	Verificacion estado; 
 	
 	List<Opinion> opiniones = new ArrayList<Opinion>();
 	
-	public Muestra(Usuario u, Foto f, Ubicacion ub) {
+	public Muestra(Usuario u, Foto f, Ubicación ub) {
 		this.autor = u;
 		this.foto = f;
 		this.estado = new OpinionBasicos();
 		this.ubicacion = ub;
+		
+		
 	}
 	public Foto getFoto() {
 		return this.foto;
 	}
-	public Ubicacion getUbicacion() {
+	public Ubicación getUbicacion() {
 		return this.ubicacion;
 	}
 	public Usuario getPersona() {
 		return this.autor;
-	}
+	} 
 	public List<Muestra> muestrasCercanas(Web w, float f){
 		// Se convierte la lista de muestras de la web en un Map con clave Ubicacion y valor Muestra:
-		Map<Ubicacion, Muestra> mapDeUbicacionMuestra = w.todasLasMuestras().stream().collect(
-				Collectors.toMap(m -> m.getUbicacion(),m -> m)
-				);
+		Map<Ubicación, Muestra> mapUM = this.muestrasPorUbicacion(w);
 		
 		// Se genera una lista con todas las ubicaciones a partir del Map:
-		List<Ubicacion> todasLasUbicaciones = new ArrayList<>(); 
-		todasLasUbicaciones.addAll(mapDeUbicacionMuestra.keySet());
+		List<Ubicación> todasLasUb = this.todasLasUbicaciones(mapUM);
 		
 		// Se obtienen las ubicaciones cercanas
-		List<Ubicacion> ubicacionesCercanas = ubicacion.ubicacionesCercanas(todasLasUbicaciones,f);
+		List<Ubicación> ubCercanas = ubicacion.ubicacionesCercanas(todasLasUb,f);
 		
 		// Se recorre la lista de ubicaciones cercanas y se genera una lista de las muestras
 		// obteniendo el valor de muestra asociado a cada clave ubicacion en el primer Map
 		List<Muestra> muestrasCercanas = new ArrayList<Muestra>();
-		for(Ubicacion ub:ubicacionesCercanas) {
-			muestrasCercanas.add(mapDeUbicacionMuestra.get(ub));
+		for(Ubicación ub:ubCercanas) {
+			muestrasCercanas.add(mapUM.get(ub));
 		}
 		
 		return muestrasCercanas; 
 	}
+	
+	protected Map<Ubicación,Muestra> muestrasPorUbicacion(Web w){
+		Map<Ubicación,Muestra> map = 
+				w.todasLasMuestras().stream().collect(
+						Collectors.toMap(
+								m -> m.getUbicacion(), 
+								m -> m)
+						);
+		return map;
+	}
+	
+	protected List<Ubicación> todasLasUbicaciones(Map<Ubicación,Muestra> map){
+		List<Ubicación> lista = new ArrayList<>();
+		lista.addAll(map.keySet());
+		return lista;
+	}
+	
 }	
--- test/web_vinchucas/MuestraTest.java
+++ test/web_vinchucas/MuestraTest.java
@@ -1,19 +1,102 @@
 package web_vinchucas;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 class MuestraTest {
-
+	
+	Muestra miMuestra;
+	
+	Usuario miUsuario;
+	Foto miFoto;
+	Ubicación miUbicación;
+	
+	Web miWeb;
+	
+	Muestra muestra_1;
+	Muestra muestra_2;
+	Muestra muestra_3;
+	
+	List<Muestra> listaDeMuestras;
+	
+	Ubicación ubicación_1;
+	Ubicación ubicación_2;
+	Ubicación ubicación_3;
+	
+	Map<Ubicación, Muestra> mapUM;
+	Map<Ubicación, Muestra> miMap;
+	
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		miUsuario = new Usuario();
+		miFoto = new Foto();
+		miUbicación = new Ubicación();
+		
+		miMuestra = new Muestra(miUsuario, miFoto, miUbicación);
+		
+		miWeb = mock(Web.class);
+		
+		muestra_1 = mock(Muestra.class);
+		muestra_2 = mock(Muestra.class);
+		muestra_3 = mock(Muestra.class);
+		
+		listaDeMuestras = new ArrayList<Muestra>();
+		listaDeMuestras.add(muestra_1);
+		listaDeMuestras.add(muestra_2);
+		listaDeMuestras.add(muestra_3);
+		
+		mapUM = new HashMap<Ubicación,Muestra>();
+		mapUM.put(ubicación_1, muestra_1);
+		mapUM.put(ubicación_2, muestra_2);
+		mapUM.put(ubicación_3, muestra_3);
+		
 	}
 
 	@Test
-	void testPrueba() {
-		fail("Not yet implemented");
+	void testGetFoto() {
+		assertEquals(miFoto,miMuestra.getFoto());
 	}
+	
+	@Test
+	void testGetPersona() {
+		assertEquals(miUsuario,miMuestra.getPersona());
+	}
+	
+	@Test
+	void testGetUbicacion() {
+		assertEquals(miUbicación,miMuestra.getUbicacion());
+	}
+	
+	@Test
+	void testMuestrasPorUbicacion() {
+		//SetUp
+		when(miWeb.todasLasMuestras()).thenReturn(listaDeMuestras);
+		when(muestra_1.getUbicacion()).thenReturn(ubicación_1);
+		when(muestra_2.getUbicacion()).thenReturn(ubicación_2);
+		when(muestra_3.getUbicacion()).thenReturn(ubicación_3);
+		
+		//Excercise
+		miMap = miMuestra.muestrasPorUbicacion(miWeb);
+		
+		//Verify
+		verify(miWeb).todasLasMuestras();
+		verify(muestra_1).getUbicacion();
+		verify(muestra_2).getUbicacion();
+		verify(muestra_3).getUbicacion();
+		
+		assertEquals(miMap, mapUM);
+	}
+	
+	
 
 }
