1c54691db1ee222e942b9ff2eef705ec6159ccbe
martinez-quimey <quimeym374@gmail.com>
2025-05-28 15:33:25 -0300

    elimine el NoVerificada del paquete en que no debia estar

--- src/verificacion/NoVerificada.java
+++ src/verificacion/NoVerificada.java
@@ -3,11 +3,11 @@ package verificacion;
 import web_vinchucas.Muestra;
 import web_vinchucas.Usuario;
 
-public class NoVerificada {
+public abstract  class NoVerificada {
 	abstract boolean esVerificada();
 	abstract boolean puedeVotar (Usuario usuario);
 	abstract void verificar(); //recalcula el tipo de verificacion
-	public NoVerificada (Muestra muestra) {
+	public NoVerificada (Muestra muestra){
 		super (muestra);
 	}
 }
