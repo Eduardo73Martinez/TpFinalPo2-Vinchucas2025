d7e34dcb7de1c6797767441ef0e2ec0acb484ac4
Lucio <Lucio@DESKTOP-MSN6PQ5>
2025-06-02 15:09:14 -0300

    Usuario y sus Test

--- src/web_vinchucas/Usuario.java
+++ src/web_vinchucas/Usuario.java
@@ -1,12 +1,80 @@
 package web_vinchucas;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
 	
-	public Usuario() {
-		
+	Web web;
+	Double id;
+	Nivel nivel;
+	List<Opinion> opiniones = new ArrayList<Opinion>();
+	List<Muestra> muestras = new ArrayList<Muestra>();
+	
+	public Usuario(Web web, Double id) {
+		this.web = web;
+		this.id = id;
+		this.nivel = Nivel.BASICO;
+	}
+	
+	public Double getId() {
+		return id;
 	}
 
+	public Nivel getNivel() {
+		return nivel;
+	}
 
+	public List<Opinion> getOpiniones() {
+		return opiniones;
+	}
 
+	public List<Muestra> getMuestras() {
+		return muestras;
+	}
+	
+	public void actualizarNivel(Nivel nuevoNivel) {
+		this.nivel = nuevoNivel;
+	}
 
+	public void opinar(Muestra m, IOpinable vo) {
+		if (m.puedeOpinar(this.nivel)) {
+			Opinion miOpinion = new Opinion(this.nivel, vo);
+			m.agregarOpinion(miOpinion);
+			this.opiniones.add(miOpinion);
+		}
+	}
+	
+	public void enviarMuestra(TipoVinchuca t, Double latitud, Double longitud) {
+		Muestra nuevaMuestra = this.crearMuestra(t, latitud, longitud);
+		this.muestras.add(nuevaMuestra);
+		web.agregarMuestra(nuevaMuestra);
+	}
+	
+	protected Muestra crearMuestra(TipoVinchuca t,Double latitud, Double longitud) {
+		TipoVinchuca vinchuca = t;
+		Foto foto = new Foto();
+		Ubicacion ubicacion = new Ubicacion(latitud,longitud);
+		Muestra nuevaMuestra = new Muestra(this,vinchuca,foto,ubicacion);
+		return nuevaMuestra;
+	}
+	
+	public List<LocalDate> getFechasRevisiones(){
+		List<LocalDate> revisiones = new ArrayList<LocalDate>();
+		for (Opinion o : this.opiniones) {
+			revisiones.add(o.getFecha());
+		}
+		return revisiones;
+	}
+	
+	public List<LocalDate> getFechasEnvios(){
+		List<LocalDate> envios = new ArrayList<LocalDate>();
+		for (Muestra m : this.muestras) {
+			envios.add(m.getFechaCreacion());
+		}
+		return envios;
+	}
+	
+	
 }
---
+++ test/web_vinchucas/UsuarioTest.java
@@ -0,0 +1,171 @@
+package web_vinchucas;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UsuarioTest {
+	
+	Usuario miUsuario;
+	
+	Web miWeb;
+	Double id;
+
+	List<Opinion> misOpiniones;
+	List<Muestra> misMuestras;
+	List<LocalDate> listaConFecha;
+	
+	Muestra muestra_1;
+	Muestra muestra_2;
+	
+	IOpinable opinable_1;
+	
+	Opinion opinion_1;
+	
+	TipoVinchuca tipoVinchuca_1;
+	
+	Double lat;
+	Double lon;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	
+		//DOC:
+		miWeb = mock(Web.class);
+		id = 3025d;
+		
+		//SUT:
+		miUsuario = new Usuario(miWeb, id);
+		
+		//DOC:
+		muestra_1 = mock(Muestra.class);
+		opinable_1 = mock(IOpinable.class);
+		opinion_1 = mock(Opinion.class);
+		
+		misOpiniones = new ArrayList<Opinion>();
+		misMuestras = new ArrayList<Muestra>();
+		
+		tipoVinchuca_1 = mock(TipoVinchuca.class);
+		lat = 30d;
+		lon = 32d;
+		
+		listaConFecha = new ArrayList<LocalDate>();
+		listaConFecha.add(LocalDate.now());
+	}
+	
+	@Test
+	void testGetId() {
+		assertEquals(miUsuario.getId(),id);
+	}
+	
+	@Test
+	void testGetNivel() {
+		assertEquals(miUsuario.getNivel(),Nivel.BASICO);
+	}
+	
+	@Test
+	void testGetOpiniones() {
+		assertEquals(miUsuario.getOpiniones(),misOpiniones);
+	}
+	
+	@Test
+	void testGetMuestras() {
+		assertEquals(miUsuario.getMuestras(),misMuestras);
+	}
+	
+	@Test
+	void testActualizarNivel() {
+		//Exercise
+		miUsuario.actualizarNivel(Nivel.EXPERTO);
+		
+		//Verify
+		assertEquals(miUsuario.getNivel(),Nivel.EXPERTO);
+	}
+	
+	@Test
+	void testOpinarCuandoPuedeOpinar() {
+		//SetUp
+		when(muestra_1.puedeOpinar(Nivel.BASICO)).thenReturn(true);
+		
+		
+		//Exercise
+		miUsuario.opinar(muestra_1,opinable_1);
+		
+		//Verify
+		assertEquals(miUsuario.getOpiniones().getFirst().getValorOpinion(),opinable_1);
+	}
+	
+	@Test
+	void testOpinarCuandoNoPuedeOpinar() {
+		//SetUp
+		when(muestra_1.puedeOpinar(Nivel.BASICO)).thenReturn(false);
+		
+		
+		//Exercise
+		miUsuario.opinar(muestra_1,opinable_1);
+		
+		//Verify
+		assertTrue(miUsuario.getOpiniones().isEmpty());
+	}
+	@Test
+	void testCrearMuestra() {
+		//Exercise
+		Muestra nuevaMuestra = miUsuario.crearMuestra(tipoVinchuca_1, lat, lon);
+		//Verify
+		assertEquals(nuevaMuestra.getVinchuca(),tipoVinchuca_1);
+		assertEquals(nuevaMuestra.getPersona(),miUsuario);
+		assertEquals(nuevaMuestra.getUbicacion().getLatitud(),lat);
+		assertEquals(nuevaMuestra.getUbicacion().getLongitud(),lon);
+		
+	}
+	
+	@Test
+	void testEnviarMuestra() {
+		
+		//Exercise
+		miUsuario.enviarMuestra(tipoVinchuca_1, lat, lon);
+		
+		//Verify
+		Muestra muestraCreada = miUsuario.getMuestras().getFirst();
+				
+		verify(miWeb).agregarMuestra(muestraCreada);
+		assertEquals(muestraCreada.getPersona(),miUsuario);
+		assertEquals(muestraCreada.getVinchuca(),tipoVinchuca_1);
+		assertEquals(muestraCreada.getUbicacion().getLatitud(),lat);
+		assertEquals(muestraCreada.getUbicacion().getLongitud(),lon);
+		
+	}
+	
+	
+	@Test
+	void testGetFechasRevisiones() {
+		//SetUp
+		when(muestra_1.puedeOpinar(Nivel.BASICO)).thenReturn(true);
+		miUsuario.opinar(muestra_1, opinable_1);
+		
+		//Exercise
+		List<LocalDate> misRevisiones = miUsuario.getFechasRevisiones();
+		
+		//Verify
+		assertEquals(misRevisiones, listaConFecha);
+	}
+	
+	@Test
+	void testGetFechasEnvios() {
+		//SetUp
+		miUsuario.enviarMuestra(tipoVinchuca_1, lat, lon);
+		
+		//Exercise
+		List<LocalDate> misEnvios = miUsuario.getFechasEnvios();
+		
+		//Verify
+		assertEquals(misEnvios, listaConFecha);
+	}
+	
+} 
