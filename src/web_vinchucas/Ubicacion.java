57e56bbec6269078c4c4e42015972e32413756df
luciernago <luciocristofanetti@gmail.com>
2025-06-05 19:59:42 -0300

    agregue getters y declare ubicacionesCercanas para que copile el UsuarioTest

--- src/web_vinchucas/Ubicacion.java
+++ src/web_vinchucas/Ubicacion.java
@@ -1,14 +1,32 @@
 package web_vinchucas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ubicacion {
-	private Double latidud;
+	private Double latitud;
 	private Double longitud;
 	
 
 	public Ubicacion(Double latitud, Double longitud) {
 		// TODO Auto-generated constructor stub
-		this.latidud = latitud;
+		this.latitud = latitud;
 		this.longitud = longitud;
 	}
+	
+	public List<Ubicacion> ubicacionesCercanas(List<Ubicacion> listaUbicaciones, float distancia){
+		List<Ubicacion> nuevaLista = new ArrayList<Ubicacion>();
+		//TODO: hacer metodo
+		return nuevaLista;
+	}
 
+	public Double getLatitud() {
+		// TODO Auto-generated method stub
+		return this.latitud;
+	}
+
+	public Double getLongitud() {
+		// TODO Auto-generated method stub
+		return this.longitud;
+	}
 }
