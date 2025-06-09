d4de5a9ed67123d90364d20c7dbf957fe437c992
Eduardo73Martinez <martin61martinez@hotmail.com.ar>
2025-06-09 11:46:28 -0300

    corrigo error en ubicacion. Saco lineas de pruebas.

--- src/ZonaOrganizanizacionUbicacion/Ubicacion.java
+++ src/ZonaOrganizanizacionUbicacion/Ubicacion.java
@@ -6,17 +6,16 @@ import java.util.List;
 public class Ubicacion {
 	private Double latitud;
 	private Double longitud;
-	
 
 	public Ubicacion(Double latitud, Double longitud) {
 		// TODO Auto-generated constructor stub
 		this.latitud = latitud;
 		this.longitud = longitud;
 	}
-	
-	public List<Ubicacion> ubicacionesCercanas(List<Ubicacion> listaUbicaciones, float distancia){
+
+	public List<Ubicacion> ubicacionesCercanas(List<Ubicacion> listaUbicaciones, float distancia) {
 		List<Ubicacion> nuevaLista = new ArrayList<Ubicacion>();
-		//TODO: hacer metodo
+		// TODO: hacer metodo
 		return nuevaLista;
 	}
 
@@ -29,31 +28,9 @@ public class Ubicacion {
 		// TODO Auto-generated method stub
 		this.longitud = longitud;
 	}
-	
-	
-	/*
-	 * 
-	 * 
-	 * java doc 
-	 * 
-	 * estoy probando agregar lineas 
-	 * paraa ver si estan 
-	 * en el grafico de
-	 * github
-	 */
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 	public Double getLatidud() {
-		return latidud;
+		return this.latitud;
 	}
 
 	public Double getLongitud() {
