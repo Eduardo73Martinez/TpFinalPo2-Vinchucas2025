668b07ff76ff6606c6e9f20aca7a342e1a32ab5c
luciernago <luciocristofanetti@gmail.com>
2025-06-10 17:10:02 -0300

    agregue atributo fechaUltimaVotacion, su getter y getVerificacion()

--- src/web_vinchucas/Muestra.java
+++ src/web_vinchucas/Muestra.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.mockito.stubbing.OngoingStubbing;
+//import org.mockito.stubbing.OngoingStubbing;
 
 public class Muestra {
 	Usuario autor;
@@ -16,7 +16,9 @@ public class Muestra {
 	Foto foto;
 	Ubicacion ubicacion;
 	LocalDate fechaCreacion;
+	LocalDate fechaUltimaVotacion;
 	Verificacion estado; 
+	
 	
 	List<Opinion> opiniones = new ArrayList<Opinion>();
 	
@@ -24,12 +26,14 @@ public class Muestra {
 		this.autor = u;
 		this.vinchuca = t;
 		this.foto = f;
-		this.estado = new OpinionBasicos(this);
+		this.estado = new OpinionBasicos();
 		this.ubicacion = ub;
 		this.fechaCreacion = LocalDate.now();
+		this.fechaUltimaVotacion = null;
 	}
+	
 	public String resultadoActual() {
-		return this.estado.resultadoActual();
+		return this.estado.resultadoActual(this);
 	}
 	public Foto getFoto() {
 		return this.foto;
@@ -49,8 +53,15 @@ public class Muestra {
 	public LocalDate getFechaCreacion() {
 		return this.fechaCreacion;
 	}
+	public LocalDate getFechaUltimaVotacion() {
+		return fechaUltimaVotacion;
+	}
 	public void agregarOpinion(Opinion o) {
 		this.opiniones.add(o);
+		this.fechaUltimaVotacion = LocalDate.now();
+	}
+	public Verificacion getVerificacion() {
+		return this.estado;
 	}
 	public void setVerificacion(Verificacion v) {
 		this.estado = v;
@@ -98,5 +109,7 @@ public class Muestra {
   
 	public boolean puedeOpinar(Usuario u) {
 		return estado.puedeVotar(u, this);
-  }
+	}
+	
+	
 }	
