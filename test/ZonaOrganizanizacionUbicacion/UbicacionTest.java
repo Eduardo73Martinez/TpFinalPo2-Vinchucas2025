ccae4c91cd40860d1be9fc535444d85d023d5d34
Eduardo73Martinez <martin61martinez@hotmail.com.ar>
2025-06-11 22:11:15 -0300

    UBICACION TERMINADA FALTARIA SABER SI IMPLEMENTO EL FILTRO DE MUESTRAS A MENOS DE TAL DISTANCIA.

--- test/ZonaOrganizanizacionUbicacion/UbicacionTest.java
+++ test/ZonaOrganizanizacionUbicacion/UbicacionTest.java
@@ -1,7 +1,5 @@
 package ZonaOrganizanizacionUbicacion;
 
-
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
@@ -13,27 +11,81 @@ import java.util.ArrayList;
 import java.util.List;
 
 class UbicacionTest {
-	private Ubicacion ubicacion;
+	private Ubicacion miubicacion;
 	private Ubicacion ubicacion2;
+	private Ubicacion ubicacion3;
+
 	private Muestra muestra1;
 	private Muestra muestra2;
 	private Muestra muestra3;
 	private Muestra[] arrayMuestras = { muestra1, muestra2, muestra3 };
+	private ArrayList<Ubicacion> ubicacionesCercanas = new ArrayList<Ubicacion>();
 
 	@BeforeEach
 	void setUp() throws Exception {
-		ubicacion = new Ubicacion(45.9, 45.5);
+		miubicacion = new Ubicacion(10.0, 45.0);
 		ubicacion2 = new Ubicacion(66.5, 46.4);
+		ubicacion3 = new Ubicacion(30.4, 50.3);
 		muestra1 = mock(Muestra.class);
 		muestra2 = mock(Muestra.class);
 		muestra3 = mock(Muestra.class);
+		
+
+		ubicacionesCercanas.add(ubicacion2);
+		ubicacionesCercanas.add(ubicacion3);
 
 	}
 
 	@Test
 	void testUbicacion() {
 
-		assertEquals(Ubicacion.class, ubicacion.getClass());
+		assertEquals(Ubicacion.class, miubicacion.getClass());
+	}
+
+	// setters y getters
+
+	@Test
+	void testGetLatitud() {
+		assertEquals(10.0, miubicacion.getLatidud());
+	}
+
+	@Test
+	void testGetLongitud() {
+		assertEquals(45.0, miubicacion.getLongitud());
+	}
+
+	@Test
+	void testSetLatitud() {
+		double nuevoValor = 43.4;
+		miubicacion.setLatitud(43.4);
+		assertEquals(nuevoValor, miubicacion.getLatidud());
+	}
+
+	@Test
+	void testSetLongitud() {
+		double nuevoValor = 23.3;
+		miubicacion.setLongitud(23.3);
+		assertEquals(nuevoValor, miubicacion.getLongitud());
+	}
+	// funcionalidades
+
+	@Test
+	void testListaUbicacionesConDistanciaMenorA() {
+		
+		List<Ubicacion> lista = miubicacion.ubicacionesCercanas(ubicacionesCercanas, 50.0);
+		
+		assertEquals(1, lista.size());
+	}
+	/**
+	 * IMPORTANTE PREGUNTAR SI IMPLEMENTO ESTA FUNCIONALIDAD 
+	 * O LA DEJO QUE LA IMPLEMENTE LA CLASE MUESTRA
+	 * 
+	 * 
+	 */
+
+	@Test
+	void testListaMuestrasObtenidasAMenosDeTalDistancia() {
+		fail("Not yet implemented");
 	}
 
 	@Test
@@ -42,40 +94,13 @@ class UbicacionTest {
 	}
 
 	@Test
-	void testListaUbicacionesConDistanciaMenorA() {
-
-		fail("Not yet implemented");
+	void testRaizCuadrada() {
+		assertEquals(4, miubicacion.raizCuadrada(16));
 	}
 
 	@Test
-	void testListaMuestrasObtenidasAMenosDeTalDistancia() {
-		fail("Not yet implemented");
-	}
-
-	// setters y getters
-
-	@Test
-	void testGetLatitud() {
-		assertEquals(45.9, ubicacion.getLatidud());
-	}
-
-	@Test
-	void testGetLongitud() {
-		assertEquals(45.5, ubicacion.getLongitud());
-	}
-
-	@Test
-	void testSetLatitud() {
-		double nuevoValor = 43.4;
-		ubicacion.setLatitud(43.4);
-		assertEquals(nuevoValor, ubicacion.getLatidud());
-	}
-
-	@Test
-	void testSetLongitud() {
-		double nuevoValor = 23.3;
-		ubicacion.setLongitud(23.3);
-		assertEquals(nuevoValor, ubicacion.getLongitud());
+	void testPotencia() {
+		assertEquals(9, miubicacion.potencia(3));
 	}
 
 }
