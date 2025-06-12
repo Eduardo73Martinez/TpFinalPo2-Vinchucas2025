a139ba3c7a4091774f49474b61703afb8eef7b6e
Eduardo73Martinez <martin61martinez@hotmail.com.ar>
2025-06-12 13:34:42 -0300

    configuro test zonaCobertura y notificacion a organizaciones.

--- src/ZonaOrganizanizacionUbicacion/ZonaCobertura.java
+++ src/ZonaOrganizanizacionUbicacion/ZonaCobertura.java
@@ -2,6 +2,10 @@ package ZonaOrganizanizacionUbicacion;
 
 public class ZonaCobertura {
 
+	public ZonaCobertura(String nombre, Ubicacion epicentro, double radioEnKilometros) {
+		// TODO Auto-generated constructor stub
+	}
+
     
 
 }
