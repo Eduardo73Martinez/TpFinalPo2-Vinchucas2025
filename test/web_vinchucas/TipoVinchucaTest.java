69e29bdda42d6816939f9712566b6c0133a67225
luciernago <luciocristofanetti@gmail.com>
2025-06-22 21:21:16 -0300

    Agregue test de TipoVinchuca

---
+++ test/web_vinchucas/TipoVinchucaTest.java
@@ -0,0 +1,23 @@
+package web_vinchucas;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TipoVinchucaTest {
+
+	TipoVinchuca miVinchuca;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		miVinchuca = TipoVinchuca.VINCHUCA_GUASAYANA;
+	}
+	
+	@Test
+	void testGetValor() {
+		//Exercise
+		String respuesta = miVinchuca.getValor();
+		assertEquals(respuesta, "Vinchuca Guasayana");
+	}
+}
