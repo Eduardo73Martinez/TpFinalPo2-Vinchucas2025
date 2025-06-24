1b178e8dfff833e9dfc2255cd1e7d95874d52e0b
luciernago <luciocristofanetti@gmail.com>
2025-06-23 21:18:52 -0300

    Volvi a poner los metodos para actualizar nivel en el Usuario y sus test

--- test/web_vinchucas/UsuarioTest.java
+++ test/web_vinchucas/UsuarioTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -24,13 +26,21 @@ public class UsuarioTest {
 	List<Muestra> misMuestrasVacia;
 	List<LocalDate> listaConFecha;
 	
-	Muestra muestra_1;
+	Muestra muestra_1;  Muestra muestra_2;  Muestra muestra_3;  Muestra muestra_4;  Muestra muestra_5;
+	Muestra muestra_6;  Muestra muestra_7;  Muestra muestra_8;  Muestra muestra_9;  Muestra muestra_10;
+	Muestra muestra_11; Muestra muestra_12; Muestra muestra_13; Muestra muestra_14; Muestra muestra_15;
+	Muestra muestra_16; Muestra muestra_17; Muestra muestra_18; Muestra muestra_19; Muestra muestra_20;
+	Muestra muestra_21;
 	
 	Ubicacion ubicacion_1;
 	
 	IOpinable opinable_1;
 	
-	Opinion opinion_1; 
+	Opinion opinion_1; Opinion opinion_2; Opinion opinion_3; Opinion opinion_4; Opinion opinion_5;
+	Opinion opinion_6; Opinion opinion_7; Opinion opinion_8; Opinion opinion_9; Opinion opinion_10;
+	Opinion opinion_11; Opinion opinion_12; Opinion opinion_13; Opinion opinion_14; Opinion opinion_15;
+	Opinion opinion_16; Opinion opinion_17; Opinion opinion_18; Opinion opinion_19; Opinion opinion_20;
+	Opinion opinion_21; 
 	
 	TipoVinchuca tipoVinchuca_1;
 	
@@ -51,8 +61,27 @@ public class UsuarioTest {
 		misOpiniones = new ArrayList<Opinion>();
 		misMuestrasVacia = new ArrayList<Muestra>();
 				
-		muestra_1 = mock(Muestra.class);
+		muestra_1  = mock(Muestra.class); muestra_2  = mock(Muestra.class); muestra_3  = mock(Muestra.class);
+		muestra_4  = mock(Muestra.class); muestra_5  = mock(Muestra.class); muestra_6  = mock(Muestra.class);
+		muestra_7  = mock(Muestra.class); muestra_8  = mock(Muestra.class); muestra_9  = mock(Muestra.class);
+		muestra_10 = mock(Muestra.class); muestra_11 = mock(Muestra.class); muestra_12 = mock(Muestra.class);
+		muestra_13 = mock(Muestra.class); muestra_14 = mock(Muestra.class); muestra_15 = mock(Muestra.class);
+		muestra_16 = mock(Muestra.class); muestra_17 = mock(Muestra.class); muestra_18 = mock(Muestra.class);
+		muestra_19 = mock(Muestra.class); muestra_20 = mock(Muestra.class); muestra_21 = mock(Muestra.class);
+		
+		misMuestras = Arrays.asList(muestra_1,  muestra_2,  muestra_3,  muestra_4,  muestra_5,
+									muestra_6,  muestra_7,  muestra_8,  muestra_9,  muestra_10, 
+									muestra_11, muestra_12, muestra_13, muestra_14, muestra_15, 
+									muestra_16, muestra_17, muestra_18, muestra_19, muestra_20,
+									muestra_21);
+		/*
+		* 
+		* intente usar un forEach para el mock pero no lo pude hacer andar
+		* misMuestras.forEach(m->m = mock(Muestra.class));
+		*/
+		
 		ubicacion_1 = mock(Ubicacion.class);
+		
 		opinable_1 = mock(IOpinable.class);
 		opinion_1 = mock(Opinion.class);
 		
@@ -63,9 +92,9 @@ public class UsuarioTest {
 		listaConFecha = new ArrayList<LocalDate>();
 		listaConFecha.add(LocalDate.now());
 		
-		when(miWeb.obtenerUbicacion(lat,lon)).thenReturn(ubicacion_1);
 		when(ubicacion_1.getLatidud()).thenReturn(lat);
 		when(ubicacion_1.getLongitud()).thenReturn(lon);
+		when(miWeb.verificarUbicacion(ubicacion_1)).thenReturn(ubicacion_1);
 	}
 	
 	@Test
@@ -176,4 +205,120 @@ public class UsuarioTest {
 		assertEquals(misEnvios, listaConFecha);
 	}
 	
+	
+	@Test
+	void testConvertirEnEspecialista() {
+		//Exercise
+		miUsuario.convertirEnEspecialista();
+		//Verify
+		assertEquals(miUsuario.getNivel(),Nivel.ESPECIALISTA);
+	}
+	
+	@Test
+	void testActualizarNivelCuandoEsEspecialista() {
+		//SetUp
+		miUsuario.convertirEnEspecialista();
+		
+		//Exercise
+		miUsuario.actualizarNivel();
+		
+		//Verify
+		assertEquals(miUsuario.getNivel(),Nivel.ESPECIALISTA);
+	}
+	
+	@Test
+	void testActualizarNivelCuandoEsBasicoYCorrespondeExperto() {
+		//SetUp
+		/* Hago que el usuario cree 11 muestras con fecha del dia de Hoy */
+		int inicio = 0; int ultimo = 11;
+		while (inicio<ultimo) { 
+			miUsuario.enviarMuestra(tipoVinchuca_1, 15d, 10d);
+			inicio++;
+		}
+		/* El usuario opina en 21 muestras
+		 * intente hacerlo con un forEach pero no lo pude hacer andar
+		*
+		*/
+		
+		misMuestras.forEach(m-> when(m.puedeOpinar(miUsuario)).thenReturn(true));
+		/*
+		when( muestra_1.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_2.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_3.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_4.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_5.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_6.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_7.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_8.puedeOpinar(miUsuario)).thenReturn(true);
+		when( muestra_9.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_10.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_11.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_12.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_13.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_14.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_15.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_16.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_17.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_18.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_19.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_20.puedeOpinar(miUsuario)).thenReturn(true);
+		when(muestra_21.puedeOpinar(miUsuario)).thenReturn(true);
+		*/
+		misMuestras.forEach(m-> miUsuario.opinar(m,opinable_1));
+		
+		//Exercise
+		miUsuario.actualizarNivel();
+		
+		//Verify
+		assertEquals(miUsuario.getNivel(),Nivel.EXPERTO);
+	}
+	
+	@Test
+	void testActualizarNivelCuandoEsBasicoYNoCumpleParaExperto() {
+		//Exercise
+		miUsuario.actualizarNivel();
+				
+		//Verify
+		assertEquals(miUsuario.getNivel(),Nivel.BASICO);
+	}
+	
+	@Test
+	void testCantidadAntesDeFechaCuandoCumplen() {
+		//Exercise
+		Long fechas = miUsuario.cantidadAntesDeFecha(listaConFecha, LocalDate.now().minus(1, ChronoUnit.DAYS));
+		
+		//Verify
+		assertEquals(fechas,1);
+	}
+	
+	@Test
+	void testCantidadAntesDeFechaCuandoNoCumplen() {
+		//SetUp
+		LocalDate referencia = LocalDate.now().plus(31, ChronoUnit.DAYS);
+		
+		//Exercise
+		Long fechas = miUsuario.cantidadAntesDeFecha(listaConFecha, referencia);
+		
+		//Verify
+		assertEquals(fechas,0);
+	}
+	
+	@Test
+	void testNivelSegunCriterioDaParaExperto() {
+		Nivel nivel;
+		//Exercise
+		nivel = miUsuario.nivelSegunCriterio(25l, 25l);
+		
+		//Verify
+		assertEquals(nivel, Nivel.EXPERTO);
+	}
+	
+	void testNivelSegunCriterioNoDaParaExperto() {
+		Nivel nivel;
+		//Exercise
+		nivel = miUsuario.nivelSegunCriterio(0l, 25l);
+		
+		//Verify
+		assertEquals(nivel, Nivel.BASICO);
+	}
 } 
